52a8c6673a719b33203a6c9074252f8a82b852d7
Awaiz Adnan <awaizadnan@gmail.com>
2024-04-22 04:36:09 +0500

    commit #

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Projects
+# Projects List
+
+- Short repo of a few projects I developed for clients !
--- auto_push_script.sh
+++ auto_push_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+COMMITCOUNT = 3
 # echo "# Projects" >> README.md
 # git init
 # git add README.md
 git add .
-git commit -m "second commit"
+git commit -m "commit # $COMMITCOUNT"
 git branch -M main
 git remote add origin https://github.com/awaiz360/Projects.git
 git push -u origin main
